17df8127ddceca47a184ce289557f242f31154e4
ncp9988@gmail.com <v.i.c@Chaus-MBP.hitronhub.home>
2021-12-26 09:11:53 -0600

    move routes to separate file

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
